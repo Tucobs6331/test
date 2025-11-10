33b71e3a5a7bd8e4c0ec3caa1bd25ca6bdcac756
tucobs6331 <docevidadojoao@gmail.com>
2025-11-10 18:34:10 -0300

    test commit

--- Trabalhando/Gerenciamento Automatico/source.py
+++ Trabalhando/Gerenciamento Automatico/source.py
@@ -4,22 +4,40 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 rodando = True
+tmpx = 60
+
+
 
 class MeuManipulador(FileSystemEventHandler):
+
+    def gitpush():
+
+
     def __init__(self):
+        self.tmprd = True
+        self.tmp = tmpx
+        self.last = time.time()
         self.a = 0
     
     def on_modified (self, event):
         print(f"[{time.ctime()}] modified {event.src_path}")
         self.a += 1
+    
+
+
     def on_deleted(self, event):
         print(f"[{time.ctime}] deleted on: {event.src_path}")
         self.a += 1
+
+
     def on_created (self, event):
         print(f"[{time.ctime}] created {event.src_path}")  
         self.a += 1
 
+        
+
 ###############################################################################################
+
 
 path = "../.." 
 
@@ -36,6 +54,7 @@ observer.schedule(event_handler, path, recursive=True)
 observer.start()
 
 print(f"ativo em: {path}")
+
 
 
 try:
